89aeab16bca81467bf18983644a38901913bbd76
everprogrammer <amir.tavakolian68@gmail.com>
2023-10-16 10:57:01 +0330

    - Optimized the restful API
    - add put  and delete to the class Item
    - use reqparser to parse the incoming requests

--- flask_RESTful_development/app.py
+++ flask_RESTful_development/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 
@@ -18,6 +18,11 @@ items = []
 # api.add_resource(Student, '/student/<string:name>')
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+            'price', type=float, required=True, help='This must be filled!'
+            )
+    
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name']==name, items), None)
@@ -27,11 +32,28 @@ class Item(Resource):
         if next(filter(lambda x: x['name']==name, items),None):
             return {'message': f"An item with name '{name}' already exists!"}, 400
         
-        data = request.get_json()
+        data = Item.parser.parse_args()
+        
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
     
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted!'}  
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.updata(data)
+        return item  
+
 class ItemList(Resource):
     def get(self):
         return {'items':items}
