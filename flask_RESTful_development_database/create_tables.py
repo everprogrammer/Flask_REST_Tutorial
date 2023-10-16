bea241af088013e1e722c44d06a2ba3b2c341e5a
everprogrammer <amir.tavakolian68@gmail.com>
2023-10-16 15:54:04 +0330

    - Add post/delete methods to Item class
    - remove data entry query from the create_tables.py
    - add post/delete methods to Item

--- flask_RESTful_development_database/create_tables.py
+++ flask_RESTful_development_database/create_tables.py
@@ -9,8 +9,6 @@ cursor.execute(table)
 table = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
 cursor.execute(table)
 
-cursor.execute("INSERT INTO items VALUES ('piano', 10.99)")
-
 connection.commit()
 
 connection.close()
--- flask_RESTful_development_database/item.py
+++ flask_RESTful_development_database/item.py
@@ -25,12 +25,49 @@ class Item(Resource):
             return {'item': {'name': row[0], 'price': row[1]}}
         return {'message': 'Item not found!'}, 404
     
-    def post(self, name):
-        pass
-    
-    def delete(self, name):
-        pass  
+    @classmethod
+    def search_by_name(cls, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
 
+        query = "SELECT * FROM items WHERE name=?"
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+        connection.close()
+
+        if row:
+            return {'item': {'name': row[0], 'price': row[1]}}
+
+    def post(self, name):
+        item = self.search_by_name(name)
+        if item:
+            return {'message': f'Item with name {name} already exists'}, 400
+        
+        data = Item.parser.parse_args()
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (name, data['price']))
+
+        connection.commit()
+        connection.close()
+
+        return {'item': {'name': name, 'price': data['price']}}, 201
+    
+
+    def delete(self, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "DELETE FROM items WHERE name=?"
+        cursor.execute(query, (name,))
+
+        connection.commit()
+        connection.close()  
+
+        return {'message': 'Item deleted!'}
     def put(self, name):
         pass
 
